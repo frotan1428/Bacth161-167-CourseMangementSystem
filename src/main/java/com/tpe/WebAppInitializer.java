8f262f338fc9ea628de73bb9036e57b5e81fd1c7
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2023-07-15 23:18:58 +0430

    Config

--- src/main/java/com/tpe/WebAppInitializer.java
+++ src/main/java/com/tpe/WebAppInitializer.java
@@ -1,4 +1,32 @@
 package com.tpe;
-public class WebAppInitializer{
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    //db and hibernate related setting
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+                RootContextConfig.class
+        };
+    }
+
+
+    //Spring MVC Config -class with Bean definition
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {
+                WebMvcConfig.class
+        };
+    }
+
+
+    //which URL will be for the servlet
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+
+    }
 }
--- src/main/java/com/tpe/domain/Course.java
+++ src/main/java/com/tpe/domain/Course.java
@@ -1,5 +1,57 @@
 package com.tpe.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "t_course")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+
+    @NotEmpty(message = "Please provide course Name :") //" "
+    private String coursename;
+
+    @NotBlank(message = "Please provide course prefix :")//""
+    private String prefix;
+
+
+    @NotNull(message = "Please provide course code :")
+    private Integer code;
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getCoursename() {
+        return coursename;
+    }
+
+    public void setCoursename(String coursename) {
+        this.coursename = coursename;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
 
 }
